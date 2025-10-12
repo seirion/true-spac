68510a8058d0769ef2e9053120534236b3589c2d
babo.neo <seirion@mensakorea.org>
2025-10-12 10:46:32 +0900

    NavHostController 를 이용한 화면 네이게이션
    androidx-navigation-compos 을 추가하면서 컴파일 오류로

--- app/src/main/java/com/trueedu/spac/MainActivity.kt
+++ app/src/main/java/com/trueedu/spac/MainActivity.kt
@@ -4,30 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.trueedu.spac.analytics.LocalTrueAnalytics
 import com.trueedu.spac.analytics.TrueAnalytics
 import com.trueedu.spac.data.config.LocalFeature
 import com.trueedu.spac.data.config.LocalFeatureConfig
+import com.trueedu.spac.ui.main.MainScreen
 import com.trueedu.spac.ui.theme.TrueSpacTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -49,65 +31,9 @@ class MainActivity : ComponentActivity() {
                 LocalFeatureConfig provides localFeature,
             ) {
                 TrueSpacTheme {
-                    TrueSpacApp()
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@PreviewScreenSizes
-@Composable
-fun TrueSpacApp() {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
-
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestinations.entries.forEach {
-                item(
-                    icon = {
-                        Icon(
-                            it.icon,
-                            contentDescription = it.label
-                        )
-                    },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
-                )
-            }
-        }
-    ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android: ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})",
-                modifier = Modifier.padding(innerPadding)
-            )
-        }
-    }
-}
-
-enum class AppDestinations(
-    val label: String,
-    val icon: ImageVector,
-) {
-    HOME("Home", Icons.Default.Home),
-    FAVORITES("Favorites", Icons.Default.Favorite),
-    PROFILE("Profile", Icons.Default.AccountBox),
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrueSpacTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/trueedu/spac/ui/home/HomeScreen.kt
@@ -0,0 +1,7 @@
+package com.trueedu.spac.ui.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+}
