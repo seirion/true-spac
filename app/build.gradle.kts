72906da41f4b19da26fb4fa435b883ec534ed3ff
babo.neo <seirion@mensakorea.org>
2025-10-13 19:09:22 +0900

    스플래시 화면 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -154,6 +154,7 @@ tasks.register("printVersionCode") {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
