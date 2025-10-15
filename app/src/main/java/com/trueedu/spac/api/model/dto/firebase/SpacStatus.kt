35a6a5c9e9544591fbdfe04bbeaf13967a2d9ad1
babo.neo <seirion@mensakorea.org>
2025-10-15 22:21:39 +0900

    spac 상태 필드 추가

--- app/src/main/java/com/trueedu/spac/api/model/dto/firebase/SpacStatus.kt
+++ app/src/main/java/com/trueedu/spac/api/model/dto/firebase/SpacStatus.kt
@@ -4,8 +4,17 @@ data class SpacStatus(
     val code: String,
     val nameKr: String,
     val redemptionPrice: Int?,
-    val status: String,
+    val status: Status,
 ) {
     // No-argument constructor required for Firebase
-    constructor() : this("000000", "", null, "")
+    constructor() : this("000000", "", null, Status.NORMAL)
+
+    enum class Status(val description: String) {
+        NORMAL("일반"),
+        MERGER_REVIEW("합병심사"),
+        MERGER_APPROVED("합병승인"),
+        DELISTING("상장폐지"),
+        UNKNOWN("-"),
+        ;
+    }
 }
