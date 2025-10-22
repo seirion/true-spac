b5df64e6663efc3ea087536838c26d3017d67145
babo.neo <seirion@mensakorea.org>
2025-10-22 21:43:16 +0900

    GH-33 dart 전자 공시 데이터 firebase 에 읽고 쓰기

---
+++ app/src/main/java/com/trueedu/spac/repo/firebase/FirebaseDartManager.kt
@@ -0,0 +1,67 @@
+package com.trueedu.spac.repo.firebase
+
+import com.google.firebase.database.GenericTypeIndicator
+import com.trueedu.spac.dart.model.DartListResponse
+import com.trueedu.spac.data.log.logD
+import com.trueedu.spac.util.toDateTimeCompactString
+import kotlinx.coroutines.tasks.await
+import java.time.LocalDateTime
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * FirebaseRealtimeDatabase 에서 dart 데이터 처리
+ */
+@Singleton
+class FirebaseDartManager @Inject constructor(
+): FirebaseDatabaseBase() {
+    companion object {
+        private const val BASE_PATH = "dart"
+        private const val CHILD_PATH = "list"
+    }
+
+    /**
+     * yyyyMMddHHmm 형식의 Long 타입 반환
+     */
+    suspend fun lastUpdatedAt(): Long {
+        val currentUser = firebaseCurrentUser() ?: run {
+            logD("lastUpdatedAt() failed: currentUser null")
+            return 0L
+        }
+        val snapshot = database.getReference("meta").get().await()
+        val lastUpdatedAt = snapshot.child("dartLastUpdatedAt").getValue(Long::class.java)
+        return lastUpdatedAt ?: 0L
+    }
+
+    suspend fun loadDartList(): List<DartListResponse> {
+        logD("loadDartList()")
+        val currentUser = firebaseCurrentUser() ?: run {
+            logD("loadDartList() failed: currentUser null")
+            return emptyList()
+        }
+        val ref = database.getReference(BASE_PATH)
+        val snapshot = ref.child(CHILD_PATH)
+        val list = snapshot.get().await()
+            .getValue(object : GenericTypeIndicator<List<DartListResponse>>() {})
+        return list ?: emptyList()
+    }
+
+    suspend fun writeDartList(list :List<DartListResponse>) {
+        logD("writeDartList(): ${list.size}")
+        val currentUser = firebaseCurrentUser() ?: run {
+            logD("writeDartList() failed: currentUser null")
+            return
+        }
+        val ref = database.getReference(BASE_PATH)
+        val snapshot = ref.child(CHILD_PATH)
+        snapshot.setValue(list).await()
+
+        val metaRef = database.getReference("meta")
+        metaRef.child("dartLastUpdatedAt").setValue(
+            LocalDateTime.now()
+                .toDateTimeCompactString()
+                .dropLast(2) // ss 제거 하여 yyyyMMddHHmm 으로 변환
+                .toLong()
+        ).await()
+    }
+}
