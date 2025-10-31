863f8cd5f9275745b24488951fa37558c6475ee9
babo.neo <seirion@mensakorea.org>
2025-11-01 00:37:26 +0900

    가격 정보 받기 조건 체크 오류 수정

--- app/src/main/java/com/trueedu/spac/api/model/dao/StockPriceDao.kt
+++ app/src/main/java/com/trueedu/spac/api/model/dao/StockPriceDao.kt
@@ -1,14 +1,14 @@
 package com.trueedu.spac.api.model.dao
 
 data class StockPriceDao(
-    val nameKr: String,
-    val price: String,
-    val priceChange: String, // 전일 대비
-    val priceChangeRate: String, // 전일 대비 등락률
-    val volumePrice: String, // 누적 거래대금
-    val volume: String, // 누적 거래량
-    val open: String, // 시가
-    val high: String, // 고가
-    val low: String, // 저가
-    val previousClosePrice: String, // 기준가(전일종가 or 시가)
+    val nameKr: String = "",
+    val price: String = "",
+    val priceChange: String = "", // 전일 대비
+    val priceChangeRate: String = "", // 전일 대비 등락률
+    val volumePrice: String = "", // 누적 거래대금
+    val volume: String = "", // 누적 거래량
+    val open: String = "", // 시가
+    val high: String = "", // 고가
+    val low: String = "", // 저가
+    val previousClosePrice: String = "", // 기준가(전일종가 or 시가)
 )
