76ed6eae9a83c42d61302ebb87ec9b1467e5ccdb
babo.neo <seirion@mensakorea.org>
2025-11-03 00:22:33 +0900

    스팩 필터 bottom sheet

---
+++ app/src/main/java/com/trueedu/spac/ui/common/OnOffSetting.kt
@@ -0,0 +1,36 @@
+package com.trueedu.spac.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.trueedu.spac.ui.components.MySwitch
+import com.trueedu.spac.ui.components.TrueText
+
+@Composable
+fun OnOffSetting(
+    title: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(horizontal = 16.dp)
+    ) {
+        TrueText(s = title, fontSize = 16, color = MaterialTheme.colorScheme.primary)
+        MySwitch(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+    }
+}
--- app/src/main/java/com/trueedu/spac/ui/home/model/SpacFilter.kt
+++ app/src/main/java/com/trueedu/spac/ui/home/model/SpacFilter.kt
@@ -3,5 +3,5 @@ package com.trueedu.spac.ui.home.model
 data class SpacFilter(
     val listedOverTwoYears: Boolean = false, // 3년 차 종목
     val underParValue: Boolean = false, // 액면가 이하 가격 종목
-    val onlyWatching: Boolean = false, // 관심 종목만 보기
+    val filterFollowing: Boolean = false, // 관심 종목만 보기
 )
