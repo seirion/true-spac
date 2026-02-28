2b1d6a0dfcc861cc605cec6cc78f6f1e8f149aa7
babo.neo <seirion@mensakorea.org>
2026-02-28 20:46:17 +0900

    2027년 공휴일 추가

--- app/src/main/java/com/trueedu/spac/util/HolidayUtils.kt
+++ app/src/main/java/com/trueedu/spac/util/HolidayUtils.kt
@@ -17,11 +17,6 @@ fun LocalDate.isHoliday(): Boolean {
 // 몇 개 안 되니까 그냥 하드 코딩
 // 주식 장이 열리지 않는 날
 private val holidays = setOf(
-    LocalDate.of(2025, 12, 31),
-    LocalDate.of(2026, 1, 1),
-    LocalDate.of(2026, 2, 16),
-    LocalDate.of(2026, 2, 17),
-    LocalDate.of(2026, 2, 18),
     LocalDate.of(2026, 3, 2),
     LocalDate.of(2026, 5, 5),
     LocalDate.of(2026, 5, 25),
@@ -34,4 +29,17 @@ private val holidays = setOf(
     LocalDate.of(2026, 10, 9),
     LocalDate.of(2026, 12, 25),
     LocalDate.of(2026, 12, 31),
+    LocalDate.of(2027, 1, 1),   // 신정
+    LocalDate.of(2027, 2, 8),   // 설날 연휴
+    LocalDate.of(2027, 2, 9),   // 대체공휴일(설날)
+    LocalDate.of(2027, 3, 1),   // 3·1절
+    LocalDate.of(2027, 5, 5),   // 어린이날
+    LocalDate.of(2027, 5, 13),  // 부처님 오신날
+    LocalDate.of(2027, 8, 16),  // 대체공휴일(광복절)
+    LocalDate.of(2027, 9, 14),  // 추석 연휴
+    LocalDate.of(2027, 9, 15),  // 추석
+    LocalDate.of(2027, 9, 16),  // 추석 연휴
+    LocalDate.of(2027, 10, 4),  // 대체공휴일(개천절)
+    LocalDate.of(2027, 10, 11), // 대체공휴일(한글날)
+    LocalDate.of(2027, 12, 27), // 대체공휴일(크리스마스)
 )
